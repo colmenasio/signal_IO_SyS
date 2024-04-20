04049b2d22becb03c091a13edc6503c9078a83fc
Colmenero <a.colmenero.fernandez@gmail.com>
2024-04-20 16:05:47 +0200

    Refactored how serdes applies encodings to ease the implementation of noise visualization

--- common/SerDes.m
+++ common/SerDes.m
@@ -97,7 +97,7 @@ classdef SerDes < handle
             try
                 str = char(bin2dec(reshape(bits, 8, [])')');
             catch ME
-                disp("Catched"+ME)
+                disp(ME)
                 disp("Deserializer error Could not parse bits to string (Im too tired to add propper error handling here)")
             end
         end
@@ -144,50 +144,54 @@ classdef SerDes < handle
                 warning("The encodings take "++" percent of the word's size, which is arguably a lot. " + ...
                     "Consider reducing the amount of encoding or increasing the amoun of bits per word")
             end
-            words = zeros(number_of_words, obj.base.n_of_bases);
+            decoded_bits = reshape(blanks(number_of_words*word_capacity), number_of_words, word_capacity);
             % processes the all but the last word. All of this words will
             % be of full length and need no padding
-            for row_i = 1:(number_of_words-1) 
-                curr_bits_indexes = (data_length*(row_i-1)+1):(data_length*row_i);
-                curr_bits = cat(2,dec2bin(word_capacity-1, length_indicator_length),bits(curr_bits_indexes));
-                encoded_bits = obj.encoding_scheme.apply_scheme(curr_bits);
-                words(row_i,:) = obj.pack_into_word(encoded_bits);
+            for row_i = 1:number_of_words
+                if row_i < number_of_words
+                    curr_bits_indexes = (data_length*(row_i-1)+1):(data_length*row_i);
+                    curr_row_bits = cat(2,dec2bin(word_capacity-1, length_indicator_length),bits(curr_bits_indexes));
+                else
+                    remaining_bits = bits((data_length*(number_of_words-1)+1):end);
+                    last_group_length = length_indicator_length+length(remaining_bits);
+                    padded_bits = obj.add_padding(remaining_bits, '0', data_length);
+                    curr_row_bits = cat(2,dec2bin(last_group_length-1, length_indicator_length),padded_bits);
+                end
+                decoded_bits(row_i, :) = curr_row_bits;
             end
             %processes the last group of bits, adding padding to it
-            remaining_bits = bits((data_length*(number_of_words-1)+1):end);
-            last_group_length = length_indicator_length+length(remaining_bits);
-            padded_bits = obj.add_padding(remaining_bits, '0', data_length);
-            curr_bits = cat(2,dec2bin(last_group_length-1, length_indicator_length),padded_bits);
-            encoded_bits = obj.encoding_scheme.apply_scheme(curr_bits);
-            words(number_of_words,:) = obj.pack_into_word(encoded_bits);
+            encoded_bits = obj.encode_bits(decoded_bits);
+            words = obj.pack_into_words(encoded_bits);
         end
 
-        function word = pack_into_word(obj, encoded_bits)
-            % Takes as an argument an array of bytes. Returns an array of
+        function words = pack_into_words(obj, encoded_bits)
+            % Takes as an argument a matrix of bytes. Returns a matrix of
             % symbols eg:[-1, 1, 3, -3] using the obj.alphabet
             % trows an error if the parity is wrong
-            try
-                groups = reshape(encoded_bits, obj.bits_per_symbol, [])';
-            catch ME
-                warning("AN ERROR OCCURED WHEN TRYING TO GROUP BITS INTO A WORD: " + ...
-                    "THERE NUMBER OF BITS IS NOT DIVISIBLE BY THE NUMBER OF BITS IN A SYMBOL")
-                rethrow(ME)
+            if  mod(length(encoded_bits), obj.bits_per_symbol) ~= 0
+                    error("AN ERROR OCCURED WHEN TRYING TO GROUP BITS INTO A WORD: " + ...
+                    "THE NUMBER OF BITS IS NOT DIVISIBLE BY THE NUMBER OF BITS IN A SYMBOL")
             end
-            n_of_symbols = height(groups);
-            word = zeros(1, n_of_symbols);
-            for symbol_i = 1:n_of_symbols
-                word(1, symbol_i) = obj.alphabet(bin2dec(groups(symbol_i, :))+1);
-            
+            n_of_symbols = length(encoded_bits)/obj.bits_per_symbol;
+            words = zeros(height(encoded_bits), n_of_symbols);
+            for row_i = 1:height(encoded_bits)
+                groups = reshape(encoded_bits(row_i, :), obj.bits_per_symbol, [])';
+                for symbol_i = 1:n_of_symbols
+                    words(row_i, symbol_i) = obj.alphabet(bin2dec(groups(symbol_i, :))+1);
+                end
             end
         end
 
         function bits = words_to_bits(obj, words)
             word_capacity = obj.base.n_of_bases * obj.bits_per_symbol;
             length_indicator_length = ceil(log2(word_capacity));
+            
+            encoded_bits = obj.unpack_from_words(words);
+            decoded_bits = obj.decode_bits(encoded_bits);
+
             bits = '';
             for row_i = 1:height(words)
-                encoded_bits = obj.unpack_from_word(words(row_i,:));
-                curr_bits = obj.encoding_scheme.unapply_scheme(encoded_bits);
+                curr_bits = decoded_bits(row_i, :);
                 message_length = bin2dec(curr_bits(1:length_indicator_length))+1;
                 data_bits_i = (length_indicator_length+1):(message_length);
                 data_bits = curr_bits(data_bits_i);
@@ -195,16 +199,20 @@ classdef SerDes < handle
             end
         end
 
-        function encoded_bits = unpack_from_word(obj, word)
-            % Takes as an argument an array of bytes. Returns an array of
-            % symbols eg:[-1, 1, 3, -3] using the obj.alphabet
+        function encoded_bits = unpack_from_words(obj, words)
+            % Takes as an argument a matrix of words (components). Returns a matrix of
+            % bits using the obj.alphabet
             % trows an error if the parity is wrong
-            n_of_bits = length(word) * obj.bits_per_symbol;
-            groups = reshape(blanks(n_of_bits), [], obj.bits_per_symbol);
-            for symbol_i = 1:length(word)
-                groups(symbol_i, :) = dec2bin(obj.find_in_alphabet(word(1, symbol_i)), obj.bits_per_symbol);
+            bits_per_word = length(words) * obj.bits_per_symbol;
+            total_n_of_bits = bits_per_word*height(words);
+            encoded_bits = reshape(blanks(total_n_of_bits), height(words), bits_per_word);
+            for row_i = 1:height(words)
+                groups = reshape(blanks(bits_per_word), [], obj.bits_per_symbol);
+                for symbol_i = 1:length(words)
+                    groups(symbol_i, :) = dec2bin(obj.find_in_alphabet(words(row_i, symbol_i)), obj.bits_per_symbol);
+                end
+                encoded_bits(row_i, :) = reshape(groups', 1, []);
             end
-            encoded_bits = reshape(groups', 1, []);
         end
         
         function signal = apply_base(obj, words)
@@ -227,6 +235,26 @@ classdef SerDes < handle
             words = zeros(height(signal), obj.base.n_of_bases);
             for word_i = 1:height(signal)
                 words(word_i, :) = obj.base.from_signal(signal(word_i,:));
+            end
+        end
+
+        function encoded_bits = encode_bits(obj, decoded_bits)
+            % Takes a matrix of data(bits) and applies the encodings to it,
+            % returning a matrix of encoded bits
+            % TODO change the assert to propper error handling
+            encoded_bits = [];
+            for row_i = 1:height(decoded_bits)
+                encoded_bits = cat(1, encoded_bits, obj.encoding_scheme.apply_scheme(decoded_bits(row_i, :)));
+            end
+        end
+
+        function decoded_bits = decode_bits(obj, encoded_bits)
+            % Takes a matrix of encoded bits and unnaplies the encodings to it,
+            % returning a matrix of data(bits)
+            % TODO change the assert to propper error handling
+            decoded_bits = [];
+            for row_i = 1:height(encoded_bits)
+                decoded_bits = cat(1, decoded_bits, obj.encoding_scheme.unapply_scheme(encoded_bits(row_i, :)));
             end
         end
 
