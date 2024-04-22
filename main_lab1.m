2b95f6d81481fd66fd0ec8af5085122396080b9f
Colmenero <a.colmenero.fernandez@gmail.com>
2024-04-22 15:36:32 +0200

    Split main into 2 mains, one for each lab session

---
+++ main_lab1.m
@@ -0,0 +1,23 @@
+%% INSTANCIATE SERDES
+
+serdes = SerDes(SinesBase(), NoneEncScheme());
+
+%% MANUAL TEST
+
+message1 = 'AAAAAAAAAAAAAAAYUDAAAAAAAAAAAAa xd';
+signal = serdes.from_str(message1);
+message2 = serdes.to_str(signal);
+assert(all(message1==message2))
+
+%% PLAY SOUND
+serdes.play_signal(signal)
+
+%% AUTOMATED TEST
+base = SinesBase();
+SerDes.auto_test_base(base, [-3, -1, 1, 3])
+
+%% CLEANUP
+clear
+clc
+
+
---
+++ main_lab2.m
@@ -0,0 +1,30 @@
+%% SETUP
+
+serdes = SerDes(SinesBase(), NoneEncScheme());
+message = 'Una matrioshka (en rusia-Ucrania: матрёшкa también llamada en español muñeca rusa, matrioska, mamushka o bábushka es un conjunto de muñecas tradicion...';
+
+%% TESTING
+
+NSR_db = 0;
+[og_bits, broken_bits] = serdes.noisyfy_bits(message, NSR_db);
+disp("Received correctly "+100*serdes.get_correctness_rate_bits(og_bits, broken_bits)+" % of the bits")
+
+%% 1)
+
+NSR_db = 2;
+
+[~, broken_components] = serdes.noisyfy_components(message, NSR_db);
+serdes.do_error_scatter(broken_components)
+
+clear broken_components NSR_db
+
+%% 2)
+nsr_range = -22:1.5:3;
+iterations_n = 2;
+serdes.do_rms_sweep_plot(message, nsr_range, iterations_n)
+
+clear nsr_range iterations_n
+
+%% Cleanup
+clc
+clear
